c1763323cd9453aeb7ce1c1acdb189856a2e3f27
Ram Ganesh K. R <83528327+Ram-20062003@users.noreply.github.com>
2022-02-28 23:58:55 +0530

    fix: snakecase to camelCase (#21)

--- player_code/java/Attributes.java
+++ player_code/java/Attributes.java
@@ -1,14 +1,14 @@
 public class Attributes {
     public final int hp;
     public final int range;
-    public final int attack_power;
+    public final int attackPower;
     public final int speed;
     public final int price;
     
-    public Attributes(int hp, int range, int attack_power, int speed, int price) {
+    public Attributes(int hp, int range, int attackPower, int speed, int price) {
         this.hp = hp;
         this.range = range;
-        this.attack_power = attack_power;
+        this.attackPower = attackPower;
         this.speed = speed;
         this.price = price;
     }
